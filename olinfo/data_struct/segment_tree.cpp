2b777663829b6030918990376945b8612ecdfd21
Angelo 'Flecart' Huang <huangelo02@gmail.com>
2022-05-16 20:06:36 +0200

    ds(olinfo): fastest segtree i got till now

--- olinfo/data_struct/segment_tree.cpp
+++ olinfo/data_struct/segment_tree.cpp
@@ -1,4 +1,4 @@
-#pragma GCC optimize("Ofast")
+#pragma GCC optimize("Ofast,unroll-loops")
 #include <vector>
 #include <algorithm>
 #include <iostream>
@@ -6,20 +6,23 @@
 using namespace std;
 typedef long long ll;
 const ll inf = 1e15;
-const int kmax = 4 * 1e6 + 5;
+const int kmax = 4 * 1e6 + 1;
 int g_time;
 
-#define left(n) (n * 2 + 1)
-#define right(n) (n * 2 + 2)
+constexpr int left(int n) {
+    return n * 2 + 1;
+}
 
-vector<pair<int, int>> ranges(kmax);
-vector<ll> mins(kmax);
-vector<ll> sums(kmax); 
-vector<ll> original(kmax);
-vector<ll> lazy_add(kmax, 0); 
-vector<ll> lazy_set(kmax, inf); 
-vector<int> time_set(kmax); 
-vector<int> time_add(kmax); 
+constexpr int right(int n) {
+    return n * 2 + 2;
+}
+
+pair<int, int> ranges[kmax];
+ll mins[kmax];
+ll sums[kmax]; 
+ll lazy[kmax]; 
+vector<bool> is_set(kmax); 
+int g_tree_size;
 int g_size;
 
 void build(int v, int l, int r, const vector<ll> &original) {
@@ -37,59 +40,39 @@ void build(int v, int l, int r, const vector<ll> &original) {
     sums[v] = sums[left(v)] + sums[right(v)];
 }
 
-// fixo il nodo in cui ha attivo sia set e add in lazy
-void fix_add_times(int v) {
-    if (lazy_add[v] == 0 or lazy_set[v] == inf) return; // devono essere entrambi attivi per avere senso
-    if (time_set[v] > time_add[v]) {
-        // just overwrite the add!
-        lazy_add[v] = 0; 
-    } else {
-        lazy_set[v] += lazy_add[v];
-        lazy_add[v] = 0; 
-    }
-}
-
 void update_add(int v) {
-    sums[v] += lazy_add[v] * (ranges[v].second - ranges[v].first + 1); 
-    mins[v] += lazy_add[v];
+    sums[v] += lazy[v] * (ranges[v].second - ranges[v].first + 1); 
+    mins[v] += lazy[v];
     int mid = (ranges[v].first + ranges[v].second) / 2;
-    if (v < sums.size() / 2) {
-        // potrebbe esserci caso in cui figlio con t_add < t_set
-        // ma viene settato da g_time a t_add > t_set, necessita di fix_add
-        fix_add_times(left(v));
-        fix_add_times(right(v));
-        lazy_add[left(v)] += lazy_add[v];
-        lazy_add[right(v)] += lazy_add[v];
-        time_add[right(v)] = time_add[left(v)] = time_add[v];
+    if (v < g_tree_size / 2) {
+        if (lazy[left(v)] == inf) lazy[left(v)] = 0;
+        if (lazy[right(v)] == inf) lazy[right(v)] = 0;
+        lazy[left(v)] += lazy[v];
+        lazy[right(v)] += lazy[v];
+        // se figli sono nodi di set, continua a settare a un valore aggiunto
+        // altrimenti la flag resta false, quindi sommo
     }
-    lazy_add[v] = 0;
+    lazy[v] = inf;
 }
 
 void update_set(int v) {
-    sums[v] = lazy_set[v] * (ranges[v].second - ranges[v].first + 1); 
-    mins[v] = lazy_set[v];
+    sums[v] = lazy[v] * (ranges[v].second - ranges[v].first + 1); 
+    mins[v] = lazy[v];
     int mid = (ranges[v].first + ranges[v].second) / 2;
-    if (v < sums.size() / 2) {
-        lazy_set[left(v)] = lazy_set[v];
-        lazy_set[right(v)] = lazy_set[v];
-        time_set[left(v)] = time_set[right(v)] = time_set[v];
+    if (v < g_tree_size / 2) {
+        lazy[left(v)] = lazy[v];
+        lazy[right(v)] = lazy[v];
+        is_set[left(v)] = is_set[right(v)] = true; 
     }
-    lazy_set[v] = inf;
+    is_set[v] = false; 
+    lazy[v] = inf;
 }
 
 void lazy_update(int v) {
-    if (lazy_add[v] == 0 and lazy_set[v] == inf) return;
-    if (lazy_set[v] == inf) {
-        update_add(v);
-        return;
+    if (lazy[v] != inf) {
+        if (is_set[v]) update_set(v);
+        else update_add(v); 
     }
-    if (lazy_add[v] == 0) {
-        update_set(v);
-        return;
-    }
-    // both active from here on:
-    fix_add_times(v);
-    update_set(v);
 }
 
 void add(int v, int tl, int tr, int l, int r, ll x) {
@@ -99,13 +82,10 @@ void add(int v, int tl, int tr, int l, int r, ll x) {
         mins[v] += x;
         sums[v] += x * (tr - tl + 1);
         if (tl != tr) {
-            // potrebbe esserci caso in cui figlio con t_add < t_set
-            // ma viene settato da g_time a t_add > t_set, necessita di fix_add
-            fix_add_times(left(v));
-            fix_add_times(right(v));
-            lazy_add[left(v)] += x;
-            lazy_add[right(v)] += x;
-            time_add[left(v)] = time_add[right(v)] = g_time++;
+            if (lazy[left(v)] == inf) lazy[left(v)] = 0;
+            if (lazy[right(v)] == inf) lazy[right(v)] = 0;
+            lazy[left(v)] += x;
+            lazy[right(v)] += x;
         }
         return;
     }
@@ -122,10 +102,9 @@ void set_range(int v, int tl, int tr, int l, int r, ll x) {
     if (l <= tl and tr <= r) {
         sums[v] = x * (tr - tl + 1); 
         mins[v] = x; 
-        lazy_add[v] = 0;
         if (tl != tr) {
-            lazy_set[left(v)] = lazy_set[right(v)] = x;
-            time_set[left(v)] = time_set[right(v)] = g_time++;
+            lazy[left(v)] = lazy[right(v)] = x;
+            is_set[left(v)] = is_set[right(v)] = true;
         }
         return;
     }
@@ -160,10 +139,10 @@ ll get_min(int v, int l, int r, int tl, int tr) {
 }
 
 
-int get_first(int v, int lv, int rv, int l, int r, ll x) {
+int lower_bound(int v, int lv, int rv, int l, int r, ll x) {
     lazy_update(v);
-    if(lv > r || rv < l) return -1;
-    if(l <= lv && rv <= r) {
+    if(lv > r or rv < l) return -1;
+    if(l <= lv and rv <= r) {
         if(mins[v] > x) return -1;
         while(lv != rv) {
             int mid = lv + (rv-lv)/2;
@@ -181,9 +160,9 @@ int get_first(int v, int lv, int rv, int l, int r, ll x) {
     }
 
     int mid = lv + (rv-lv)/2;
-    int rs = get_first(left(v), lv, mid, l, r, x);
+    int rs = lower_bound(left(v), lv, mid, l, r, x);
     if(rs != -1) return rs;
-    return get_first(right(v), mid+1, rv, l, r, x);
+    return lower_bound(right(v), mid+1, rv, l, r, x);
 }
 
 ll get_sum(int l, int r) {
@@ -199,11 +178,14 @@ ll get_min(int l, int r) {
     return get_min(0, l, r - 1, 0, g_size - 1);
 }
 int lower_bound(int l, int r, ll x) {
-    return get_first(0, 0, g_size - 1, l, r - 1, x);
+    return lower_bound(0, 0, g_size - 1, l, r - 1, x);
 }
 
 void init(vector<long long> a) {
+    for (ll &i : lazy) i = inf;
+
     g_size = a.size(); 
+    g_tree_size = (a.size() * 4);
     build(0, 0, g_size - 1, a);
     g_time = 0;
 }
@@ -215,7 +197,6 @@ int main() {
 	
 	int n, q;
 	cin >> n >> q;
-	
 	vector<long long> a(n);
 	for (int i = 0; i < n; i++)
 		cin >> a[i];
