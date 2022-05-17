da473781aff71c1a93c6963bd8661d0ab4a7e43c
Angelo 'Flecart' Huang <huangelo02@gmail.com>
2022-05-17 12:47:26 +0200

    ds(olinfo): final segtree

--- olinfo/data_struct/segment_tree.cpp
+++ olinfo/data_struct/segment_tree.cpp
@@ -1,4 +1,4 @@
-#pragma GCC optimize("O2,unroll-loops")
+// #pragma GCC optimize("O2,unroll-loops")
 #include <vector>
 #include <algorithm>
 #include <iostream>
@@ -19,34 +19,31 @@ constexpr int right(int n) {
 struct node {
     ll mmin;
     ll sum;
+    ll x; // the value 
+    bool is_set; // either set, or add
+    node() {}
     node (ll mmin, ll sum) {
         this->mmin = mmin;
         this->sum = sum;
+        x = inf;
+        is_set = false; 
     }
 
-    node (ll coso) {
-        mmin = sum = coso;
-    }
-    node() {}
+    node (ll coso): node(coso, coso) {}
 
     void operator +=(const node &other) {
         mmin = min(mmin, other.mmin);
         sum += other.sum;
     }
-
-    node operator +(const node &other) const {
-        return {min(mmin, other.mmin), sum + other.sum};
+    void merge(const node &first, const node &second) {
+        mmin = min(first.mmin, second.mmin);
+        sum = first.sum + second.sum;
     }
 };
 
-struct lazy_node {
-    ll x; // the value 
-    bool is_set; // either set, or add
-};
 
 pair<int, int> ranges[kmax];
 node nodes[kmax];
-lazy_node lazy[kmax];
 int g_tree_size;
 int g_size;
 int l, r; // gli l e r della chiamata!
@@ -61,38 +58,33 @@ void build(int v, int l, int r, const vector<ll> &original) {
     int mid = (l + r) / 2;
     build(left(v), l, mid, original);
     build(right(v), mid + 1, r, original);
-    nodes[v] = nodes[left(v)] + nodes[right(v)];
+    nodes[v].merge(nodes[left(v)], nodes[right(v)]);
 }
-
-void update_add(int v) {
-    nodes[v].sum += lazy[v].x * (ranges[v].second - ranges[v].first + 1); 
-    nodes[v].mmin += lazy[v].x;
-    if (v < g_tree_size / 2) {
-        if (lazy[left(v)].x == inf) lazy[left(v)].x = 0;
-        if (lazy[right(v)].x == inf) lazy[right(v)].x = 0;
-        lazy[left(v)].x += lazy[v].x;
-        lazy[right(v)].x += lazy[v].x;
-        // se figli sono nodi di set, continua a settare a un valore aggiunto
-        // altrimenti la flag resta false, quindi sommo
-    }
-    lazy[v].x = inf;
-}
-
-void update_set(int v) {
-    nodes[v].sum = lazy[v].x * (ranges[v].second - ranges[v].first + 1); 
-    nodes[v].mmin = lazy[v].x;
-    if (v < g_tree_size / 2) {
-        lazy[left(v)] = lazy[v];
-        lazy[right(v)] = lazy[v];
-        lazy[left(v)].is_set = lazy[right(v)].is_set = true;
-    }
-    lazy[v] = {inf, false};
-}
-
 void lazy_update(int v) {
-    if (lazy[v].x != inf) {
-        if (lazy[v].is_set) update_set(v);
-        else update_add(v); 
+    if (nodes[v].x != inf) {
+        if (nodes[v].is_set) {
+            nodes[v].sum = nodes[v].x * (ranges[v].second - ranges[v].first + 1); 
+            nodes[v].mmin = nodes[v].x;
+            if (v < g_tree_size / 2) {
+                nodes[left(v)] = nodes[v];
+                nodes[right(v)] = nodes[v];
+                nodes[left(v)].is_set = nodes[right(v)].is_set = true;
+            }
+            nodes[v].x = inf;
+            nodes[v].is_set = false; 
+        } else {
+            nodes[v].sum += nodes[v].x * (ranges[v].second - ranges[v].first + 1); 
+            nodes[v].mmin += nodes[v].x;
+            if (v < g_tree_size / 2) {
+                if (nodes[left(v)].x == inf) nodes[left(v)].x = 0;
+                if (nodes[right(v)].x == inf) nodes[right(v)].x = 0;
+                nodes[left(v)].x += nodes[v].x;
+                nodes[right(v)].x += nodes[v].x;
+                // se figli sono nodi di set, continua a settare a un valore aggiunto
+                // altrimenti la flag resta false, quindi sommo
+            }
+            nodes[v].x = inf;
+        }
     }
 }
 
@@ -102,10 +94,10 @@ void add(int v, int tl, int tr, ll x) {
         nodes[v].mmin += x;
         nodes[v].sum += x * (tr - tl + 1);
         if (tl != tr) {
-            if (lazy[left(v)].x == inf) lazy[left(v)].x = 0;
-            if (lazy[right(v)].x == inf) lazy[right(v)].x = 0;
-            lazy[left(v)].x += x;
-            lazy[right(v)].x += x;
+            if (nodes[left(v)].x == inf) nodes[left(v)].x = 0;
+            if (nodes[right(v)].x == inf) nodes[right(v)].x = 0;
+            nodes[left(v)].x += x;
+            nodes[right(v)].x += x;
         }
         return;
     }
@@ -114,7 +106,7 @@ void add(int v, int tl, int tr, ll x) {
     int mid = (tl + tr) / 2;
     add(left(v), tl, mid, x);
     add(right(v), mid + 1, tr, x);
-    nodes[v] = nodes[left(v)] + nodes[right(v)];
+    nodes[v].merge(nodes[left(v)], nodes[right(v)]);
 }
 
 void set_range(int v, int tl, int tr, ll x) {
@@ -123,8 +115,8 @@ void set_range(int v, int tl, int tr, ll x) {
         nodes[v].mmin = x;
         nodes[v].sum = x * (tr - tl + 1);
         if (tl != tr) {
-            lazy[left(v)].x = lazy[right(v)].x = x;
-            lazy[left(v)].is_set = lazy[right(v)].is_set = true;
+            nodes[left(v)].x = nodes[right(v)].x = x;
+            nodes[left(v)].is_set = nodes[right(v)].is_set = true;
         }
         return;
     }
@@ -133,7 +125,7 @@ void set_range(int v, int tl, int tr, ll x) {
     lazy_update(right(v));
     set_range(left(v), tl, (tl + tr) / 2, x);
     set_range(right(v), (tl + tr) / 2 + 1, tr, x);
-    nodes[v] = nodes[left(v)] + nodes[right(v)];
+    nodes[v].merge(nodes[left(v)], nodes[right(v)]);
 }
 
 ll get_sum(int v, int tl, int tr) {
@@ -187,34 +179,27 @@ int lower_bound(int v, int lv, int rv, ll x) {
 }
 
 ll get_sum(int a, int b) {
-    lazy_update(0);
     l = a; r = b - 1;
     return get_sum(0, 0, g_size - 1);
 }
 void add(int a, int b, ll x) {
-    lazy_update(0);
     l = a; r = b - 1;
     add(0, 0, g_size - 1, x);
 }
 void set_range(int a, int b, ll x) {
-    lazy_update(0);
     l = a; r = b - 1;
     set_range(0, 0, g_size - 1, x);
 }
 ll get_min(int a, int b) {
-    lazy_update(0);
     l = a; r = b - 1;
     return get_min(0, 0, g_size - 1);
 }
 int lower_bound(int a, int b, ll x) {
-    lazy_update(0);
     l = a; r = b - 1;
     return lower_bound(0, 0, g_size - 1, x);
 }
 
 void init(vector<long long> a) {
-    for (lazy_node &i : lazy) i.x = inf;
-
     g_size = a.size(); 
     g_tree_size = min(kmax, (int)(a.size() * 4));
     build(0, 0, g_size - 1, a);
